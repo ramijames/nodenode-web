63e8b79fae5e8238cc4fbe110418a85ac02b2211
Rami James <ramijames@gmail.com>
2021-09-24 20:11:08 +0300

    Shiny

--- components/HomeContent.vue
+++ components/HomeContent.vue
@@ -4,7 +4,7 @@
             <Logo/>
             <h1>Free, high-quality services to make our networks the best they can be</h1>
             <h3>Nodenode uses the proceeds from our block production activities to fund access to critical services that make for more attractive and usable products that run on our favorite open networks.</h3>
-            
+            <img src="@/assets/woosh.svg" class="woosh" preserveAspectRatio="none" />
 
 
        </div>
@@ -14,7 +14,13 @@
 <style lang="scss">
 
 #HomeContent {
-    
+    .woosh {
+        height:120%;
+        width:50%;
+        position:absolute;
+        right:0;
+        mix-blend-mode: overlay;
+    }
 }
 
 </style>
