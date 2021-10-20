ccdcc5f44af88ad556525778f641301c3df979e7
Rami James <ramijames@gmail.com>
2021-10-20 09:28:45 +0300

    user faces

--- components/ServicesContent.vue
+++ components/ServicesContent.vue
@@ -7,36 +7,25 @@
 
             <h1>Our service to the network</h1>
             <h2>Currently, we are providing these services to qualifying projects.</h2>
-            <table>
-               <tr>
-                   <td>
-                        <nuxt-link to="/schedule">
-                            <p><strong>Design ➝</strong></p>
-                            <p>Your UX is solid, but you need a more unique face</p>
-                        </nuxt-link>
-                   </td>
-                   <td>
-                        <nuxt-link to="/schedule">
-                            <p><strong>Branding ➝</strong></p>
-                            <p>Your idea is amazing, but your project needs name recognition</p>
-                        </nuxt-link>
-                   </td>
-               </tr>
-               <tr>
-                   <td>
-                        <nuxt-link to="/schedule">
-                            <p><strong>User Experience ➝</strong></p>
-                            <p>Your code is great, but nobody can figure out what to do</p>
-                        </nuxt-link>
-                   </td>
-                   <td>
-                        <nuxt-link to="/schedule">
-                            <p><strong>Product Marketing ➝</strong></p>
-                            <p>You're ready, but the market doesn't know it</p>
-                        </nuxt-link>
-                   </td>
-               </tr>
-           </table>
+
+            <ul>
+                <li>
+                        <h3><strong>Design</strong></h3>
+                        <p>Your UX is solid, but you need a more unique face</p>
+                </li>
+                <li>
+                        <h3><strong>Branding</strong></h3>
+                        <p>Your idea is amazing, but your project needs name recognition</p>
+                </li>
+                <li>
+                        <h3><strong>User Experience</strong></h3>
+                        <p>Your code is great, but nobody can figure out what to do</p>
+                </li>
+                <li>
+                        <h3><strong>Product Marketing</strong></h3>
+                        <p>You're ready, but the market doesn't know it</p>
+                </li>
+            </ul>
        </div>
     </section>
 </template>
