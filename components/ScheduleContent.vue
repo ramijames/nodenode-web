e699451934d952d30ea4b08e55e1ad448de8816e
Rami James <ramijames@gmail.com>
2021-10-28 09:29:55 +0300

    Calendly support

--- components/ScheduleContent.vue
+++ components/ScheduleContent.vue
@@ -8,8 +8,18 @@
             <h1>Schedule a free consultation</h1>
             <h2>Let’s start with an introductory call.</h2>
             <h3>As our resources are limited, we must carefully select which projects we partner with. Our criteria for acceptance are decided by the larger impact on the Telos ecosystem, user growth potential, and project complexity.</h3>
+
+            <vue-calendly url="https://calendly.com/rami-james" :height="660"></vue-calendly>
     </section>
 </template>
+
+<script>
+
+export default {
+
+}
+
+</script>
 
 <style lang="scss">
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vue-calendly.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
